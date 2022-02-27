58fbe9a2781f7380282d11b31412db0c644e811f
ArdhyanX <100517062+ArdhyanX@users.noreply.github.com>
2022-02-27 19:49:49 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+	<head>
+		<title>[!] Hacked By ArdhyanX [!]</title>
+		<!-- desc -->
+		<meta property="og:title" content="Hacked By ArdhyanX">
+		<meta property="og:description" content="$ Party At Your System $" />
+		<!-- desc -->
+
+		<!-- Fonts -->
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400&display=swap" rel="stylesheet">
+		<!-- Fonts -->
+
+		<style>
+			body {
+				margin: 0;
+				padding: 0;
+				text-align: center;
+				font-family: 'Oswald', sans-serif;
+				color: #fff;
+			}
+			img {
+				margin-top: 100px;
+			}
+			h2 {
+				margin-top: -20px;
+			}
+		</style>
+	</head>
+	<body bgcolor="black">
+		<img src="https://e.top4top.io/p_2207wabv01.gif" width="300">
+		<h1>$ When I'm Sad I Party On Your System $</h1>
+		<h2>$ Hacked By <font color="red">ArdhyanX</font> $</h2>
+		<marquee width="20%" direction="left">PalangkarayaCodeXploiter - KalimantanCyberGhost - IndonesiaSecurityNetwork - Pangkoh6etar - NaziCyberFamily - P5XPloiterCrew</marquee>
+		<p>[!] Indonesia Hacker Rulez [!]</p>
+	</body>
+</html>
